sdowhy/stripe-android

Address element private beta

paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/activity/PaymentSheetPlaygroundActivity.kt
@@ -1,7 +1,10 @@
 package com.stripe.android.paymentsheet.example.playground.activity
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.annotation.NonNull
@@ -14,10 +17,14 @@ import androidx.lifecycle.lifecycleScope
 import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.idling.CountingIdlingResource
 import com.google.android.material.snackbar.Snackbar
+import com.stripe.android.PaymentConfiguration
 import com.stripe.android.core.model.CountryCode
 import com.stripe.android.core.model.CountryUtils
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResult
+import com.stripe.android.paymentsheet.addresselement.AddressDetails
+import com.stripe.android.paymentsheet.addresselement.AddressLauncher
+import com.stripe.android.paymentsheet.addresselement.AddressLauncherResult
 import com.stripe.android.paymentsheet.example.R
 import com.stripe.android.paymentsheet.example.Settings
 import com.stripe.android.paymentsheet.example.databinding.ActivityPaymentSheetPlaygroundBinding
@@ -29,7 +36,6 @@ import com.stripe.android.paymentsheet.example.playground.viewmodel.PaymentSheet
 import com.stripe.android.paymentsheet.model.PaymentOption
 import kotlinx.coroutines.launch
 import java.util.Locale
-
 
 class PaymentSheetPlaygroundActivity : AppCompatActivity() {
     private val viewBinding by lazy {
@@ -101,8 +107,14 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
     private val setDelayedPaymentMethods: Boolean
         get() = viewBinding.allowsDelayedPaymentMethodsRadioGroup.checkedRadioButtonId == R.id.allowsDelayedPaymentMethods_on_button
 
+    private val settings by lazy {
+        Settings(this)
+    }
+
     private lateinit var paymentSheet: PaymentSheet
     private lateinit var flowController: PaymentSheet.FlowController
+    private lateinit var addressLauncher: AddressLauncher
+    private var shippingAddress: AddressDetails? = null
 
     @Nullable
     private var multiStepUIReadyIdlingResource: CountingIdlingResource? = null
@@ -130,7 +142,7 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
             ::onPaymentOption,
             ::onPaymentSheetResult
         )
-        val backendUrl = Settings(this).playgroundBackendUrl
+        addressLauncher = AddressLauncher(this, ::onAddressLauncherResult)
 
         viewBinding.currencySpinner.adapter =
             ArrayAdapter(
@@ -186,7 +198,7 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
                     linkEnabled,
                     setShippingAddress,
                     setAutomaticPaymentMethods,
-                    backendUrl
+                    settings.playgroundBackendUrl
                 )
             }
         }
@@ -198,6 +210,12 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
         viewBinding.customCheckoutButton.setOnClickListener {
             flowController.confirm()
         }
+
+        viewBinding.shippingAddressButton.setOnClickListener {
+            startShippingAddressCollection()
+        }
+
+        viewBinding.shippingAddressContainer.visibility = View.GONE
 
         viewBinding.paymentMethod.setOnClickListener {
             flowController.presentPaymentOptions()
@@ -357,6 +375,64 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
         }
     }
 
+    private fun startShippingAddressCollection() {
+        val builder = AddressLauncher.Configuration.Builder()
+        builder.googlePlacesApiKey(settings.googlePlacesApiKey)
+        if (viewBinding.shippingAddressDefaultRadioGroup.checkedRadioButtonId ==
+            viewBinding.shippingAddressDefaultOnButton.id) {
+            builder.defaultValues(
+                AddressLauncher.DefaultAddressDetails(
+                    name = "Theo Parker",
+                    address = PaymentSheet.Address(
+                        city = "South San Francisco",
+                        country = "United States",
+                        line1 = "354 Oyster Point Blvd",
+                        state = "CA",
+                        postalCode = "94080",
+                    ),
+                    phoneNumber = "5555555555",
+                    isCheckboxSelected = true
+                )
+            )
+        }
+        if (viewBinding.shippingAddressCountriesGroup.checkedRadioButtonId ==
+            viewBinding.shippingAddressCountriesPartialButton.id) {
+            builder.allowedCountries(
+                setOf("US", "CA", "AU", "GB", "FR", "JP", "KR")
+            )
+        }
+        val phone = when (viewBinding.shippingAddressPhoneRadioGroup.checkedRadioButtonId) {
+            viewBinding.shippingAddressPhoneRequiredButton.id -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.REQUIRED
+            }
+            viewBinding.shippingAddressPhoneOptionalButton.id -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.OPTIONAL
+            }
+            viewBinding.shippingAddressPhoneHiddenButton.id -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.HIDDEN
+            }
+            else -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.OPTIONAL
+            }
+        }
+        val checkboxLabel = if (viewBinding.shippingAddressCheckboxLabel.text.isNotBlank()) {
+            viewBinding.shippingAddressCheckboxLabel.text.toString()
+        } else {
+            getString(R.string.stripe_paymentsheet_address_element_same_as_shipping)
+        }
+        builder.additionalFields = AddressLauncher.AdditionalFieldsConfiguration(
+            phone = phone,
+            checkboxLabel = checkboxLabel
+        )
+        if (viewBinding.shippingAddressButtonTitle.text.isNotBlank()) {
+            builder.buttonTitle(viewBinding.shippingAddressButtonTitle.text.toString())
+        }
+        addressLauncher.present(
+            publishableKey = PaymentConfiguration.getInstance(this).publishableKey,
+            configuration = builder.build()
+        )
+    }
+
     private fun configureCustomCheckout() {
         val clientSecret = viewModel.clientSecret.value ?: return
 
@@ -401,6 +477,7 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
             customer = viewModel.customerConfig.value,
             googlePay = googlePayConfig,
             defaultBillingDetails = defaultBilling,
+            shippingDetails = shippingAddress,
             allowsDelayedPaymentMethods = viewBinding.allowsDelayedPaymentMethodsOnButton.isChecked,
             appearance = appearance
         )
@@ -440,6 +517,30 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
         }
 
         viewModel.status.value = paymentResult.toString()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun onAddressLauncherResult(addressLauncherResult: AddressLauncherResult) {
+        window.setSoftInputMode(
+            WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        )
+        viewBinding.shippingAddressContainer.visibility = View.VISIBLE
+        when (addressLauncherResult) {
+            is AddressLauncherResult.Succeeded -> {
+                shippingAddress = addressLauncherResult.address
+                val address = addressLauncherResult.address.address
+                viewBinding.shippingAddressName.text = addressLauncherResult.address.name
+                viewBinding.shippingAddressDetails.text = address?.let {
+                    "${address.line1}\n${address.city}, ${address.state}, ${address.country}"
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    viewBinding.playground.scrollToDescendant(viewBinding.shippingAddressDetails)
+                }
+            }
+            is AddressLauncherResult.Canceled -> {
+                viewBinding.shippingAddressContainer.visibility = View.GONE
+            }
+        }
     }
 
     /**

chipMrx/stripe-android-x

paymentsheet-example/src/main/java/com/stripe/android/paymentsheet/example/playground/activity/PaymentSheetPlaygroundActivity.kt
@@ -1,7 +1,10 @@
 package com.stripe.android.paymentsheet.example.playground.activity
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.annotation.NonNull
@@ -14,10 +17,14 @@ import androidx.lifecycle.lifecycleScope
 import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.idling.CountingIdlingResource
 import com.google.android.material.snackbar.Snackbar
+import com.stripe.android.PaymentConfiguration
 import com.stripe.android.core.model.CountryCode
 import com.stripe.android.core.model.CountryUtils
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResult
+import com.stripe.android.paymentsheet.addresselement.AddressDetails
+import com.stripe.android.paymentsheet.addresselement.AddressLauncher
+import com.stripe.android.paymentsheet.addresselement.AddressLauncherResult
 import com.stripe.android.paymentsheet.example.R
 import com.stripe.android.paymentsheet.example.Settings
 import com.stripe.android.paymentsheet.example.databinding.ActivityPaymentSheetPlaygroundBinding
@@ -29,7 +36,6 @@ import com.stripe.android.paymentsheet.example.playground.viewmodel.PaymentSheet
 import com.stripe.android.paymentsheet.model.PaymentOption
 import kotlinx.coroutines.launch
 import java.util.Locale
-
 
 class PaymentSheetPlaygroundActivity : AppCompatActivity() {
     private val viewBinding by lazy {
@@ -101,8 +107,14 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
     private val setDelayedPaymentMethods: Boolean
         get() = viewBinding.allowsDelayedPaymentMethodsRadioGroup.checkedRadioButtonId == R.id.allowsDelayedPaymentMethods_on_button
 
+    private val settings by lazy {
+        Settings(this)
+    }
+
     private lateinit var paymentSheet: PaymentSheet
     private lateinit var flowController: PaymentSheet.FlowController
+    private lateinit var addressLauncher: AddressLauncher
+    private var shippingAddress: AddressDetails? = null
 
     @Nullable
     private var multiStepUIReadyIdlingResource: CountingIdlingResource? = null
@@ -130,7 +142,7 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
             ::onPaymentOption,
             ::onPaymentSheetResult
         )
-        val backendUrl = Settings(this).playgroundBackendUrl
+        addressLauncher = AddressLauncher(this, ::onAddressLauncherResult)
 
         viewBinding.currencySpinner.adapter =
             ArrayAdapter(
@@ -186,7 +198,7 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
                     linkEnabled,
                     setShippingAddress,
                     setAutomaticPaymentMethods,
-                    backendUrl
+                    settings.playgroundBackendUrl
                 )
             }
         }
@@ -198,6 +210,12 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
         viewBinding.customCheckoutButton.setOnClickListener {
             flowController.confirm()
         }
+
+        viewBinding.shippingAddressButton.setOnClickListener {
+            startShippingAddressCollection()
+        }
+
+        viewBinding.shippingAddressContainer.visibility = View.GONE
 
         viewBinding.paymentMethod.setOnClickListener {
             flowController.presentPaymentOptions()
@@ -357,6 +375,69 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
         }
     }
 
+    private fun startShippingAddressCollection() {
+        val builder = AddressLauncher.Configuration.Builder()
+        builder.googlePlacesApiKey(settings.googlePlacesApiKey)
+        if (viewBinding.shippingAddressDefaultRadioGroup.checkedRadioButtonId ==
+            viewBinding.shippingAddressDefaultOnButton.id) {
+            builder.address(
+                AddressDetails(
+                    name = "Theo Parker",
+                    address = PaymentSheet.Address(
+                        city = "South San Francisco",
+                        country = "United States",
+                        line1 = "354 Oyster Point Blvd",
+                        state = "CA",
+                        postalCode = "94080",
+                    ),
+                    phoneNumber = "5555555555",
+                    isCheckboxSelected = true
+                )
+            )
+        }
+        if (viewBinding.shippingAddressCountriesGroup.checkedRadioButtonId ==
+            viewBinding.shippingAddressCountriesPartialButton.id) {
+            builder.allowedCountries(
+                setOf("US", "CA", "AU", "GB", "FR", "JP", "KR")
+            )
+        }
+        val phone = when (viewBinding.shippingAddressPhoneRadioGroup.checkedRadioButtonId) {
+            viewBinding.shippingAddressPhoneRequiredButton.id -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.REQUIRED
+            }
+            viewBinding.shippingAddressPhoneOptionalButton.id -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.OPTIONAL
+            }
+            viewBinding.shippingAddressPhoneHiddenButton.id -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.HIDDEN
+            }
+            else -> {
+                AddressLauncher.AdditionalFieldsConfiguration.FieldConfiguration.OPTIONAL
+            }
+        }
+        val checkboxLabel = if (viewBinding.shippingAddressCheckboxLabel.text.isNotBlank()) {
+            viewBinding.shippingAddressCheckboxLabel.text.toString()
+        } else {
+            getString(R.string.stripe_paymentsheet_address_element_same_as_shipping)
+        }
+        builder.additionalFields(
+            AddressLauncher.AdditionalFieldsConfiguration(
+                phone = phone,
+                checkboxLabel = checkboxLabel
+            )
+        )
+        if (viewBinding.shippingAddressButtonTitle.text.isNotBlank()) {
+            builder.buttonTitle(viewBinding.shippingAddressButtonTitle.text.toString())
+        }
+        if (viewBinding.shippingAddressTitle.text.isNotBlank()) {
+            builder.title(viewBinding.shippingAddressTitle.text.toString())
+        }
+        addressLauncher.present(
+            publishableKey = PaymentConfiguration.getInstance(this).publishableKey,
+            configuration = builder.build()
+        )
+    }
+
     private fun configureCustomCheckout() {
         val clientSecret = viewModel.clientSecret.value ?: return
 
@@ -401,6 +482,7 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
             customer = viewModel.customerConfig.value,
             googlePay = googlePayConfig,
             defaultBillingDetails = defaultBilling,
+            shippingDetails = shippingAddress,
             allowsDelayedPaymentMethods = viewBinding.allowsDelayedPaymentMethodsOnButton.isChecked,
             appearance = appearance
         )
@@ -440,6 +522,35 @@ class PaymentSheetPlaygroundActivity : AppCompatActivity() {
         }
 
         viewModel.status.value = paymentResult.toString()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun onAddressLauncherResult(addressLauncherResult: AddressLauncherResult) {
+        window.setSoftInputMode(
+            WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
+        )
+        viewBinding.shippingAddressContainer.visibility = View.VISIBLE
+        when (addressLauncherResult) {
+            is AddressLauncherResult.Succeeded -> {
+                shippingAddress = addressLauncherResult.address
+                val address = addressLauncherResult.address.address
+                viewBinding.shippingAddressName.text = addressLauncherResult.address.name
+                viewBinding.shippingAddressDetails.text = address?.let {
+                    """
+                        ${address.line1}
+                        ${address.city},
+                        ${address.state}, ${address.country}, ${address.postalCode}
+                        ${shippingAddress?.phoneNumber}
+                    """.trimIndent()
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    viewBinding.playground.scrollToDescendant(viewBinding.shippingAddressDetails)
+                }
+            }
+            is AddressLauncherResult.Canceled -> {
+                viewBinding.shippingAddressContainer.visibility = View.GONE
+            }
+        }
     }
 
     /**